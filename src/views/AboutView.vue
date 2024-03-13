2ae9974affbdb87f80b7aee3fa80f07ac77b9a74
RosyrRais <syr050301@qq.com>
2024-03-13 14:39:22 +0800

    chore(daisyUI): 完成了配置

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,7 @@
 <template>
- <div class="text-3xl underline bg-yellow-500">你好</div>
+  <div class="text-3xl underline bg-yellow-500">你好</div>
+  <button
+  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+    Button
+  </button>
 </template>
