ab4f892a50e7f87d71c81385951ab1e8081874da
RosyrRais <syr050301@qq.com>
2024-04-08 17:56:33 +0800

    feat(jobDatabase): 新增了地区选择功能

--- src/views/jobDatabase/search.vue
+++ src/views/jobDatabase/search.vue
@@ -25,12 +25,17 @@
           </select>
         </div>
         <div>
-          <span>地址</span> <input class="input input-primary input-sm float-right w-200" v-model="address">
+          <span>地址</span>
+          <div 
+          class="rounded-md border border-primary float-right w-200 h-32 flex justify-center items-center cursor-pointer" 
+          @click="() => showModal('adress_modal')"
+          >{{ address === "" ? '点击选择' : address }}</div>
         </div>
+        <div class="btn btn-sm btn-primary float-end" @click="clearFrom">清空</div>
       </div>
     </div>
-    <div v-if="totalPageNum < pageNum" @click="() => {pageNum = totalPageNum}" class="cursor-pointer">
-      该页数过大 点击返回末页
+    <div v-if="totalPageNum < pageNum || totalPageNum === 0" @click="() => {pageNum = totalPageNum}" class="cursor-pointer">
+      {{ totalPageNum === 0 ? '暂无该条件数据' : '该页数过大 点击返回末页'}}
     </div>
     <table class="table" v-else>
       <thead>
@@ -55,53 +60,22 @@
       </tbody>
     </table>
   </div>
-  <dialog id="job_detail_modal" class="modal ">
+  <dialog id="adress_modal" class="modal">
     <div class="modal-box p-20 bg-base-200">
-      <h3 class="font-bold text-2xl">{{ modalJobData.title }}</h3>
-      <div class="flex flex-col gap-10 mt-15">
-      <div class="m-3">
-        <span class="font-bold text-xl">公司名称:</span>
-        <span class="float-right text-lg">{{ modalJobData.company }}</span>
-      </div>
-      <div class="m-3">
-        <span class="font-bold text-xl">薪资条件:</span>
-        <span class="float-right text-lg">{{ modalJobData.salary }}</span>
-      </div>
-      <div class="m-3">
-        <span class="font-bold text-xl">学历要求:</span>
-        <span class="float-right text-lg">{{ modalJobData.education }}</span>
-      </div>
-      <div class="m-3">
-        <span class="font-bold text-xl">联系人:</span>
-        <span class="float-right text-lg">{{ modalJobData.hiring_manager }}</span>
-      </div>
-      <div class="m-3">
-        <span class="font-bold text-xl">地址:</span>
-        <span class="float-right text-lg">{{ modalJobData.address }}</span>
-      </div>
-      </div>
-      <div class="m-3 my-10">
-        <span class="font-bold text-xl ">技能要求:</span>
-        <span class="float-right text-xs p-6 mt-4">{{ modalJobData.description }}</span>
-      </div>
-      <div class="m-3 ">
-        <a class="btn-link font-bold mt-6" :href="modalJobData.link">详情链接</a>
-      </div>
-      <div class="divider my-6"></div>
-      <span class="text-xl m-3">评论:</span>
-      <ul class="menu bg-base-200  rounded-box">
-        <div v-for="item in commentList">
-          <div class="divider my-3"></div>
-          <li><a><span class="text-xl">{{item.user_name}}</span>: &ensp;{{item.content}}</a></li>
+      <div class="flex flex-row h-400">
+        <div class="overflow-y-scroll w-170">
+          <div 
+          v-for="(prov, index) in areaList.index" 
+          :class="['cursor-pointer', provId === index as unknown as string ? 'text-accent' : undefined]"
+          @click="() => selectProv(index)"
+          >{{ prov }}</div>
         </div>
-      </ul>
-      <div class="divider my-6 "></div>
-      <div class="flex justify-between mt-10">
-        <input class="input input-bordered w-9/12 shadow-lg" v-model="comment"/>
-        <button class="btn shadow-lg bg-base-100" @click="setComment" ><span>发布评论</span></button>
+        <div class="grid grid-cols-2">
+          <div v-for="city in areaList[provId]" class="btn btn-ghost" @click="() => selectCity(city)">{{ city }}</div>
+        </div>
       </div>
       <div class="modal-action">
-        <button class="btn bg-base-100" @click="showModal('job_detail_modal', true)">关闭</button>
+        <button class="btn bg-base-100" @click="showModal('adress_modal', true)">关闭</button>
       </div>
     </div>
   </dialog>
@@ -118,14 +92,12 @@
 </template>
 
 <script setup lang="ts">
-import {provide, ref, watch} from 'vue';
+import { ref, watch } from 'vue';
 import { useMainStore } from '@/stores';
 import { useRequest } from 'vue-hooks-plus';
-import {checkJobDatabaseAPI, getCommentAPI, setCommentAPI} from '@/apis';
-import Index from "@/views/login/index.vue";
-import {ElNotification} from "element-plus";
-import Info from "@/views/jobDatabase/info.vue";
+import { checkJobDatabaseAPI } from '@/apis';
 import router from "@/router";
+import areaList from "@/tool/area";
 
 const loginStore = useMainStore().useLoginStore();
 const pageNum = ref(1);
@@ -136,25 +108,27 @@ const company = ref("");
 const title = ref("");
 const education = ref("");
 const address = ref("");
-const comment = ref();
-const commentList = ref();
-const modalJobData = ref({
-  id: -1,
-  title: "",
-  company: "",
-  salary: "",
-  education: "",
-  hiring_manager: "",
-  address: "",
-  description: "",
-  link: "",
-})
+const provId = ref("0");
+
+const selectCity = (city: string) => {
+  address.value = city;
+  showModal('adress_modal', true);
+}
+
+const selectProv = (index: any) => {
+  provId.value = index;
+}
+
+const clearFrom = () => {
+  company.value = "";
+  title.value = "";
+  education.value = "";
+  address.value = "";
+}
 
 const checkDetail = (job: any) => {
-  modalJobData.value = job;
-  const id = modalJobData.value.id.toString()
+  const id = job.id.toString()
   localStorage.setItem( 'id' , id )
-  console.log(modalJobData.value.id)
   router.push('/info')
 }
 
@@ -179,14 +153,6 @@ const checkJobDatabase = () => {
 }
 checkJobDatabase();
 
-const showDetail = (index: number) => {
-  if(index === jobShowDetailIndex.value) {
-    jobShowDetailIndex.value = undefined;
-  } else {
-    jobShowDetailIndex.value = index;
-  }
-}
-
 const switchPageNum = (num: number) => {
   if(typeof pageNum.value === 'string' )
     pageNum.value = parseInt(pageNum.value, 10);
@@ -195,38 +161,6 @@ const switchPageNum = (num: number) => {
   }
 }
 
-
-const setComment = () => {
-  useRequest(() => setCommentAPI({
-    job_id: modalJobData.value.id,
-    parent_id:0,
-    content:comment.value
-  },loginStore.token as string),{
-    onSuccess(res: any) {
-      if(res.code === 200 && res.msg === 'OK') {
-        ElNotification.success("评论成功")
-        comment.value = ''
-        useRequest(() => getCommentAPI({
-          job_id: modalJobData.value.id,
-          page_num: pageNum.value,
-          page_size: 4
-        },loginStore.token as string),{
-          onSuccess(res: any) {
-            if(res.code === 200) {
-              commentList.value = res.data
-              console.log(commentList.value)
-            }
-          }
-        })
-      }else{
-        ElNotification.error(res.msg);
-      }
-    },
-    onError(e:any){
-      ElNotification.error("网络错误"+e)
-    }
-  })
-}
 watch(pageNum, () => {
   checkJobDatabase();
   jobShowDetailIndex.value = undefined;
