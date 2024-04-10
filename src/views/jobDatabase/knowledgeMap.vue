9a1df63e5f72eb9805e71be1f4cc70c589c9fac7
yzyshimisi <3198685096@qq.com>
2024-04-10 15:38:36 +0800

    merge

--- src/views/jobDatabase/knowledgeMap.vue
+++ src/views/jobDatabase/knowledgeMap.vue
@@ -14,9 +14,9 @@ function draw() {
     const config = {
         containerId: "viz",
         neo4j: {
-            serverUrl: "http://121.43.236.83:7474/browser/",
+            serverUrl: "neo4j+s://82c8fcea.databases.neo4j.io",
             serverUser: "neo4j",
-            serverPassword: "12345678",
+            serverPassword: "j8UBJi9twhESJ4KZjHumYRXHcYC1NnfwZZu3ItDzw4Y",
         },
         // labels: {
         //     Character: {
@@ -33,11 +33,11 @@ function draw() {
         //         }
         //     }
         // },
-        relationships: {
-            INTERACTS: {
-                value: "weight"
-            }
-        },
+        // relationships: {
+        //     INTERACTS: {
+        //         value: "weight"
+        //     }
+        // },
         initialCypher: "MATCH (n)-[r:INTERACTS]->(m) RETURN *"
     };
 
